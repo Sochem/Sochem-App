fc8794d48cb2db372580ddbada31f67a8e9969ad
Arjun Gupta <arjupta.90@gmail.com>
2021-09-01 19:25:53 +0530

    Integrating Firebase & new Splash Screen

---
+++ lib/screen/splashscreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:sochem/utils/constants.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Image(image: AssetImage(SochemIcon)),
+        ),
+      ),
+    );
+  }
+}
