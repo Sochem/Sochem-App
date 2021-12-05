beea0fb657f3ae316e81f55539a8b197c3a91d56
ItsAdityaKSingh <adityakksingh23@gmail.com>
2021-12-05 13:34:39 +0530

    commit on homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sochem/screen/SplashScreen.dart';
-import 'package:sochem/screen/home_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
