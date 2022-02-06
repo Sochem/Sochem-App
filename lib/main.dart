6a5de722e9f625dccd0df4b505e3cf534a767c3a
ItsAdityaKSingh <adityakksingh23@gmail.com>
2022-02-07 01:00:34 +0530

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:sochem/screen/cloud.dart';
 import 'package:sochem/screen/splash_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
-  runApp(MaterialApp(home: CloudPage()));
+  runApp(MaterialApp(home: App()));
 }
 
 /// We are using a StatefulWidget such that we only create the [Future] once,
