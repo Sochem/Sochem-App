00a8507689d7bbc7b8f8c390a14b8fbcfcc91ff9
ItsAdityaKSingh <adityakksingh23@gmail.com>
2022-03-29 10:41:27 +0530

    feed final

--- lib/screen/feed.dart
+++ lib/screen/feed.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -6,6 +7,8 @@ import 'package:sochem/models/post_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_html/flutter_html.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../utils/constants.dart';
 
@@ -18,9 +21,11 @@ class _FeedScreenState extends State<FeedScreen> {
   //Posts ViewModel
   List<Post> _posts = [];
   Future<List<Post>> fetchPosts() async {
-    var response =
-        await http.get(Uri.parse('https://api.npoint.io/bf243fd49e6b8783f02b'));
-
+    var response = await http
+        .get(Uri.parse("https://api.sochem.org/api/events/"), headers: {
+      HttpHeaders.authorizationHeader:
+          'Token 262132f6ee56aba6dcdc9e7bd28ed1409fb45c98'
+    });
     List<Post> posts = [];
     if (response.statusCode == 200) {
       var postsJson = json.decode(response.body);
@@ -43,11 +48,6 @@ class _FeedScreenState extends State<FeedScreen> {
   }
 
   Widget _buildPost(int index) {
-    // var postCreatedAt = DateTime(
-    //   _posts[index].timeAgo.year,
-    //   _posts[index].timeAgo.month,
-    //   _posts[index].timeAgo.day,
-    // );
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
       child: Container(
@@ -81,21 +81,19 @@ class _FeedScreenState extends State<FeedScreen> {
                           child: Image(
                             height: 50.0,
                             width: 50.0,
-                            image: AssetImage(_posts[index].authorImageUrl),
+                            image: AssetImage('assets/sochem.jpeg'),
                             fit: BoxFit.cover,
                           ),
                         ),
                       ),
                     ),
                     title: Text(
-                      _posts[index].authorName,
+                      'SoChem',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    subtitle: Text(DateFormat.yMMMMd('en_US')
-                        .format(DateTime.now())
-                        .toString()),
+                    subtitle: Text(_posts[index].date),
                     //TODO: Adding sharing option for posts
 
                     // trailing: IconButton(
@@ -122,11 +120,19 @@ class _FeedScreenState extends State<FeedScreen> {
                       child: Center(
                         child: Hero(
                           tag: "$index",
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10.0),
-                            child: CachedNetworkImage(
-                              fit: BoxFit.contain,
-                              imageUrl: _posts[index].imageUrl,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              image: DecorationImage(
+                                  image: _posts[index].cover1 != null ||
+                                          _posts[index].cover2 != null
+                                      ? NetworkImage(
+                                          _posts[index].cover1 != null
+                                              ? _posts[index].cover1
+                                              : _posts[index].cover2)
+                                      : AssetImage('assets/sochem.jpeg')
+                                          as ImageProvider,
+                                  fit: BoxFit.cover),
                             ),
                           ),
                         ),
@@ -141,12 +147,15 @@ class _FeedScreenState extends State<FeedScreen> {
                     ),
                   ),
                   Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      _posts[index].postHeading,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20.0,
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                    child: Center(
+                      child: Text(
+                        _posts[index].title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20.0,
+                        ),
                       ),
                     ),
                   )
@@ -224,32 +233,124 @@ class _FeedScreenState extends State<FeedScreen> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            CachedNetworkImage(
-              imageUrl: _posts[index].imageUrl,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Center(
-              child: Text(
-                _posts[index].postHeading,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20.0,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 25.0),
+          child: Column(
+            children: [
+              _posts[index].cover2 != null
+                  ? CachedNetworkImage(
+                      imageUrl: _posts[index].cover2,
+                    )
+                  : Image.asset('assets/sochem.jpeg'),
+              SizedBox(
+                height: 10.0,
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Text(
+                    _posts[index].title,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20.0,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
-              child: Text(_posts[index].postDesc),
-            ),
-          ],
+              SizedBox(
+                height: 5.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 15.0, vertical: 15.0),
+                child: Html(data: _posts[index].description),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  children: [
+                    _posts[index].file1 != null
+                        ? TextButton(
+                            onPressed: () {
+                              launch(_posts[index].file1);
+                            },
+                            child: Container(
+                              width: 200,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    cardImageColor,
+                                    Color.fromARGB(255, 128, 203, 196),
+                                  ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.black12,
+                                    offset: Offset(5, 5),
+                                    blurRadius: 10,
+                                  )
+                                ],
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'View File',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        : _posts[index].file2 != null
+                            ? TextButton(
+                                onPressed: () {
+                                  launch(_posts[index].file2);
+                                },
+                                child: Container(
+                                  width: 200,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                      colors: [
+                                        cardImageColor,
+                                        Color.fromARGB(255, 128, 203, 196),
+                                      ],
+                                      begin: Alignment.topLeft,
+                                      end: Alignment.bottomRight,
+                                    ),
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black12,
+                                        offset: Offset(5, 5),
+                                        blurRadius: 10,
+                                      )
+                                    ],
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      'View Second File',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            : Text(''),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
