20696919390720f9ea435e81f4987e2a3b9cf8e6
ItsAdityaKSingh <adityakksingh23@gmail.com>
2022-03-30 17:13:59 +0530

    final

--- lib/screen/profile.dart
+++ lib/screen/profile.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:shape_of_view_null_safe/shape_of_view_null_safe.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../models/people_model.dart';
 import '../utils/constants.dart';
+import 'package:http/http.dart' as http;
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -11,15 +15,39 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  List<People> _people = [];
+  Future<List<People>> fetchPeople() async {
+    var response = await http
+        .get(Uri.parse("https://api.sochem.org/api/family-list/"), headers: {
+      HttpHeaders.authorizationHeader:
+          'Token 262132f6ee56aba6dcdc9e7bd28ed1409fb45c98'
+    });
+    List<People> people = [];
+    if (response.statusCode == 200) {
+      var peopleJson = json.decode(response.body);
+      for (var peoplesJson in peopleJson) {
+        people.add(People.fromJson(peoplesJson));
+      }
+    }
+    return people;
+  }
+
   String userEmail = '';
   String userName = '';
   String year = '';
   String initials = '';
+  String house = '';
+  String rollNo = '';
 
   @override
   void initState() {
     super.initState();
-    _initialising();
+    fetchPeople().then((value) {
+      setState(() {
+        _people.addAll(value);
+        _initialising();
+      });
+    });
   }
 
   void _initialising() async {
@@ -30,6 +58,15 @@ class _ProfilePageState extends State<ProfilePage> {
       print(userName + "dev");
       initials = userName.substring(0, 1).toUpperCase();
       year = userEmail.substring(userEmail.length - 14, userEmail.length - 12);
+      for (var i = 0; i < _people.length; i++) {
+        if (_people[i].email == userEmail) {
+          prefs.setString('house', _people[i].house!);
+          prefs.setString('roll', _people[i].rollNo!);
+          break;
+        }
+      }
+      house = prefs.getString('house')!;
+      rollNo = prefs.getString('roll')!;
     });
   }
 
@@ -92,7 +129,7 @@ class _ProfilePageState extends State<ProfilePage> {
               Column(
                 children: [
                   SizedBox(
-                    height: height * 0.41,
+                    height: height * 0.35,
                   ),
                   Center(
                     child: Text(
@@ -107,7 +144,7 @@ class _ProfilePageState extends State<ProfilePage> {
                   Column(
                     children: [
                       Divider(
-                        height: 25,
+                        height: 15,
                       ),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -147,7 +184,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                       ),
                       SizedBox(
-                        height: 10,
+                        height: 5,
                       ),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -191,7 +228,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                       ),
                       SizedBox(
-                        height: 10,
+                        height: 5,
                       ),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -213,7 +250,7 @@ class _ProfilePageState extends State<ProfilePage> {
                               Padding(
                                 padding: const EdgeInsets.only(left: 15.0),
                                 child: Icon(
-                                  Icons.format_quote_outlined,
+                                  Icons.contacts_outlined,
                                   size: 35,
                                 ),
                               ),
@@ -221,7 +258,7 @@ class _ProfilePageState extends State<ProfilePage> {
                                 child: Padding(
                                   padding: EdgeInsets.fromLTRB(30, 5, 10, 5),
                                   child: Text(
-                                    "Some deep motivational dialogue who no one follows",
+                                    "Roll Number: " + rollNo,
                                     style: TextStyle(fontSize: 16),
                                   ),
                                 ),
@@ -231,7 +268,47 @@ class _ProfilePageState extends State<ProfilePage> {
                         ),
                       ),
                       SizedBox(
-                        height: 10,
+                        height: 5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Container(
+                          height: 80,
+                          width: MediaQuery.of(context).size.height * 0.7,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(30.0),
+                            ),
+                            gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                              colors: [Color(0xFFFFECDF), kBackgroundColor],
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Icon(
+                                  Icons.speaker_group_outlined,
+                                  size: 35,
+                                ),
+                              ),
+                              Flexible(
+                                child: Padding(
+                                  padding: EdgeInsets.fromLTRB(30, 5, 10, 5),
+                                  child: Text(
+                                    "You belong to " + house,
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5,
                       ),
                       Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 10),
