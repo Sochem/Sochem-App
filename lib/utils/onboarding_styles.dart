f82f3d417daf54dadcf164b2318eb51707aa57b7
Aditya Kumar Singh <adityakksingh23@gmail.com>
2022-04-01 15:49:06 +0530

    Onboarding Final (#50)

--- lib/utils/onboarding_styles.dart
+++ lib/utils/onboarding_styles.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-final kTitleStyle = TextStyle(
-  color: Colors.white,
-  fontFamily: 'CM Sans Serif',
-  fontSize: 25.0,
+final kTitleStyle = GoogleFonts.raleway(
+  textStyle: TextStyle(
+    fontSize: 25,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  ),
 );
 
-final kSubtitleStyle = TextStyle(
-  color: Colors.white,
-  fontSize: 18.0,
-  height: 1.2,
+final kSubtitleStyle = GoogleFonts.montserrat(
+  textStyle: TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+    color: Colors.black,
+  ),
 );
